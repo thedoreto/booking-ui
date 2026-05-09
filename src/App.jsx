0519b24bfaa39b7cc3dc0aa53494d6f06b9f3e46
thedoreto <the.doreto@gmail.com>
2026-05-09 10:40:21 +0300

    test URL

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
+import { useEffect, useState } from "react";
+
 function App() {
-    const [rooms] = useState([
-        { id: 1, name: "Room A" },
-        { id: 2, name: "Room B" },
-        { id: 3, name: "Room C" }
-    ]);
+    const [rooms, setRooms] = useState([]);
+
+    useEffect(() => {
+        fetch("https://booking-system-1-jfv3.onrender.com/allRooms")
+            .then((res) => res.json())
+            .then((data) => setRooms(data))
+            .catch((err) => console.error("Error:", err));
+    }, []);
 
     return (
-        <div>
+        <div style={{ padding: "20px", fontFamily: "Arial" }}>
             <h1>Rooms</h1>
 
-            {rooms.map((r) => (
-                <div key={r.id}>
-                    {r.name}
+            {rooms.map((room) => (
+                <div
+                    key={room.id}
+                    style={{
+                        border: "1px solid #ccc",
+                        padding: "10px",
+                        marginBottom: "10px",
+                        borderRadius: "8px"
+                    }}
+                >
+                    <p>Room number: {room.roomNumber}</p>
+                    <p>Type: {room.type}</p>
+                    <p>Price: {room.pricePerNight} €</p>
                 </div>
             ))}
         </div>
     );
 }
+
+export default App;
